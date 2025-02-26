268c01ec9609a1e5c85555b562d69bf5ad212494
Armaan-Virk <virkarmaan2004@gmail.com>
2025-02-25 21:59:15 -0700

    final

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
         <Link href="/week-2">Go to Week 2 Assignment</Link>
         <Link href="/week-3">Go to Week 3 Assignment</Link>
         <Link href="/week-4">Go to Week 4 Assignment</Link>
+        <Link href="/week-5">Go to Week 5 Assignment</Link>
       </p>
      
     </main>
