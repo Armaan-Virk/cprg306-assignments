8cc03af6b079ab4ea54137c92a1bbb966ed36f65
Armaan-Virk <virkarmaan2004@gmail.com>
2025-03-04 20:07:55 -0700

    Add shopping list feature with sorting functionality

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
         <Link href="/week-3">Go to Week 3 Assignment</Link>
         <Link href="/week-4">Go to Week 4 Assignment</Link>
         <Link href="/week-5">Go to Week 5 Assignment</Link>
+        <Link href="/week-6">Go to Week 6 Assignment</Link>
       </p>
      
     </main>
